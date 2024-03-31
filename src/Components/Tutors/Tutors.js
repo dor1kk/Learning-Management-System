12bc9c0d510d78cdc4be9a20c1db3e835646774c
unknown <om64842@ubt-uni.net>
2024-03-31 03:50:59 +0200

    Tutor

--- src/Components/Tutors/Tutors.js
+++ src/Components/Tutors/Tutors.js
@@ -3,7 +3,68 @@ import './Tutors.css';
 
 function Tutors(){
     return (
-        <h1>Tutors</h1>
+        <><h1>Tutors</h1>
+        <div className="box-container">
+        <div className="box">
+            <h3>Become a tutor</h3>
+            <p>HEY ................ </p>
+            <a href="#" className="inline-btn">GET STARTED</a>
+</div>
+<div className="box">
+    <div className="tutor">
+        <img className="g" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
+        <div>
+        <h3>Orgesa Morina</h3>
+        <h5>Developer</h5>
+        </div>
+        </div>
+    <p>Playlists:<span>4</span></p>
+    <p>Total videos:<span>4</span></p>
+    <p>Total likes:<span>4</span></p>
+    <a href="#" className="inline-btn">View Profile</a></div>
+   
+    <div className="box">
+    <div className="tutor">
+        <img className="g" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
+        <div>
+        <h3>Orgesa Morina</h3>
+        <h5>Developer</h5>
+        </div>
+        </div>
+    <p>Playlists:<span>4</span></p>
+    <p>Total videos:<span>4</span></p>
+    <p>Total likes:<span>4</span></p>
+    <a href="#" className="inline-btn">View Profile</a></div>
+   
+    <div className="box">
+    <div className="tutor">
+        <img className="g" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
+        <div>
+        <h3>Orgesa Morina</h3>
+        <h5>Developer</h5>
+        </div>
+        </div>
+    <p>Playlists:<span>4</span></p>
+    <p>Total videos:<span>4</span></p>
+    <p>Total likes:<span>4</span></p>
+    <a href="#" className="inline-btn">View Profile</a></div>
+   
+    <div className="box">
+    <div className="tutor">
+        <img className="g" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
+        <div>
+        <h3>Orgesa Morina</h3>
+        <h5>Developer</h5>
+        </div>
+        </div>
+    <p>Playlists:<span>4</span></p>
+    <p>Total videos:<span>4</span></p>
+    <p>Total likes:<span>4</span></p>
+    <a href="#" className="inline-btn">View Profile</a></div>
+    </div>
+
+</>
+
 
     );
 }
