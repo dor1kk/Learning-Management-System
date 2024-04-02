8d6ad0dd090ba6e222205ed3316b0b5cfa3843bf
unknown <om64842@ubt-uni.net>
2024-04-02 15:27:44 +0200

    update

--- src/Components/Tutors/Tutors.js
+++ src/Components/Tutors/Tutors.js
@@ -1,106 +1,44 @@
-import React from "react";
-import './Tutors.css';
+import React, { useState } from "react";
+import "./Tutors.css";
 import { useNavigate } from "react-router-dom";
+import tutorsData from "./Tutorsdata";
 
+function Tutors() {
+  const navigate = useNavigate();
+  const [tutors, setTutors] = useState(tutorsData);
 
-function Tutors(){
-    const navigate=useNavigate();
-    return (
-        <><h1>Tutors</h1>
-        <div className="box-container">
+  return (
+    <>
+      <h1>Tutors</h1>
+      <div className="box-container">
         <div className="box">
-            <h3>Become a tutor</h3>
-            <p>HEY ................ </p>
-        <button className="inline-btn" onClick={()=>navigate("/TutorProfile ")}>Get started</button>
-</div>
-<div className="box">
-        <div className="g" >
-        <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
+          <h3>Become a tutor</h3>
+          <p>HEY ................ </p>
+          <button className="inline-btn" onClick={() => navigate("/TutorProfile ")}>
+            Get started
+          </button>
         </div>
-    <div className="p">
-            <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-            
-       </div>   <span style={{color:"orange"}}>&#9733;&#9733;&#9733;&#9733;&#9733; </span>
-        <h5>Orgesa Morina</h5>
-        <p>Courses <span>4</span></p>
-    <button className="inline-btn" onClick={()=>navigate("/TutorProfile ")}>View profile</button>
-</div>
-<div className="box">
-        <div className="g" >
-        <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-        </div>
-    <div className="p">
-            <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-            
-       </div>   <span style={{color:"orange"}}>&#9733;&#9733;&#9733;&#9733;&#9733; </span>
-       <h5>Orgesa Morina</h5>
-        <p>Courses <span>4</span></p>
-    <button className="inline-btn" onClick={()=>navigate("/TutorProfile ")}>View profile</button>
-</div>
-<div className="box">
-        <div className="g" >
-        <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-        </div>
-    <div className="p">
-            <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-            
-       </div>   <span style={{color:"orange"}}>&#9733;&#9733;&#9733;&#9733;&#9733; </span>
-       <h5>Orgesa Morina</h5>
-        <p>Courses <span>4</span></p>
-    <button className="inline-btn" onClick={()=>navigate("/TutorProfile ")}>View profile</button>
-</div>
-<div className="box">
-        <div className="g" >
-        <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-        </div>
-    <div className="p">
-            <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-            
-       </div>   <span style={{color:"orange"}}>&#9733;&#9733;&#9733;&#9733;&#9733; </span>
-        <h5>Orgesa Morina</h5>
-        <p>Courses <span>4</span></p>
-    <button className="inline-btn" onClick={()=>navigate("/TutorProfile ")}>View profile</button>
-</div>
 
-<div className="box">
-        <div className="g" >
-        <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-        </div>
-    <div className="p">
-            <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-            
-       </div> <span style={{color:"orange"}}>&#9733;&#9733;&#9733;&#9733;&#9733;</span>
-       <h5>Orgesa Morina</h5>
-        <p>Courses <span>4</span></p>
-    <button className="inline-btn" onClick={()=>navigate("/TutorProfile ")}>View profile</button>
-</div>
-
-<div className="box">
-        <div className="g" >
-        <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-        </div>
-    <div className="p">
-            <img  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f4bd7a6-f763-4518-9b81-bdfd40ce3fc9/d26yer1-421bb5b8-9fc2-4d5a-b2d1-1e1f81b26b82.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVmNGJkN2E2LWY3NjMtNDUxOC05YjgxLWJkZmQ0MGNlM2ZjOVwvZDI2eWVyMS00MjFiYjViOC05ZmMyLTRkNWEtYjJkMS0xZTFmODFiMjZiODIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p5vfqGmq9kIylfG3glHGa20CAPUtoWlAxKEGpIvGOi8" alt=""></img>
-            
-       </div>   <span style={{color:"orange"}}>&#9733;&#9733;&#9733;&#9733;&#9733; </span>
-       <h6>Orgesa Morina</h6>
-        <p>Courses <span>4</span></p>
-    <button className="inline-btn" onClick={()=>navigate("/TutorProfile ")}>View profile</button>
-</div>
-</div>
-    
-
-
-
-    
-   
-    
-    
-
-</>
-
-
-    );
+        {tutors.map(tutor => (
+          <div className="box" key={tutor.id}>
+            <div className="g">
+              <img src={tutor.image1} alt="" />
+            </div>
+            <div className="p">
+              <img src={tutor.image2} alt="" />
+            </div>
+            <span style={{ color: "orange" }}>&#9733;&#9733;&#9733;&#9733;&#9733; </span>
+            <h5>{tutor.name}</h5>
+            <p>Courses <span>{tutor.courses}</span></p>
+            <p>Rating <span>{tutor.rating}</span></p>
+            <button className="inline-btn" onClick={() => navigate("/TutorProfile ")}>
+              View profile
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Tutors;
---
+++ src/Components/Tutors/Tutorsdata.js
@@ -0,0 +1,86 @@
+const tutors = [
+    {
+      id: "1",
+      name: "Maryjo Delaguila",
+      image2: "https://xsgames.co/randomusers/assets/avatars/female/21.jpg",
+      rating: 5,
+      courses: 4,
+    },
+    {
+      id: "2",
+      name: "John Doe",
+      image1: "",
+      image2: "https://xsgames.co/randomusers/assets/avatars/male/21.jpg",
+      rating: 4,
+      courses: 3,
+    },
+    {
+      id: "3",
+      name: "Fatos Kryeziu",
+      image1: "",
+      image2: "https://media.licdn.com/dms/image/C4D03AQE2OdtrZmwLKA/profile-displayphoto-shrink_800_800/0/1657506748846?e=2147483647&v=beta&t=_ojUs-yH30nDdOM8FuM2DnzETHuIbB_76CA5_VxhYcI",
+      rating: 4.5,
+      courses: 4,
+    },
+    
+    {
+      id: "4",
+      name: "Adrola Dushi",
+      image1: "",
+      image2: "https://ucreatestudio.com/wp-content/uploads/2013/02/female-CEO-headshot.jpg",
+      rating: 4.8,
+      courses: 6,
+    },
+
+    {
+      id: "5",
+      name: "Enver Gashi",
+      image1: "",
+      image2: "https://media.licdn.com/dms/image/D5603AQFMV2hXARA-cg/profile-displayphoto-shrink_800_800/0/1691193350837?e=2147483647&v=beta&t=sUVUpRP7i29yLozKdEXrYSvluEAco-7yTN7NvMbUXMk",
+      rating: 4.9,
+      courses: 5,
+    },
+    {
+      id: "6",
+      name: "Anela Berisha",
+      image1: "",
+      image2: "https://headshots-inc.com/wp-content/uploads/2023/03/CEO-Headshot-Examples-3-1.jpg",
+      rating: 4.5,
+      courses: 5,
+    },
+    {
+      id: "7",
+      name: "Hung Dowd",
+      image1: "",
+      image2: "https://www.hcpc-uk.org/globalassets/image-library/about-us/people/bernie-ceo-profile-1.jpg",
+      rating: 4.2,
+      courses: 3,
+    },
+    {
+      id: "8",
+      name: "Willian Hidy",
+      image1: "",
+      image2: "https://i.pinimg.com/736x/07/33/ba/0733ba760b29378474dea0fdbcb97107.jpg",
+      rating: 4.5,
+      courses: 5,
+    },
+    {
+      id: "9",
+      name: "Luana Santiano",
+      image1: "",
+      image2: "https://headshots-inc.com/wp-content/uploads/2023/03/CEO-Headshot-Examples-2-1.jpg",
+      rating: 4.7,
+      courses: 6,
+    },
+    {
+      id: "10",
+      name: "Beula Mohd",
+      image1: "",
+      image2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGZBWqYsefx95NKSysIdEdkhEi0-d5QK9vX9VsijxsPSUhqXQkorPNV3qIa2RT0TNnu04&usqp=CAU",
+      rating: 4.3,
+      courses: 4,
+    },
+  ];
+  
+  export default tutors;
+  
